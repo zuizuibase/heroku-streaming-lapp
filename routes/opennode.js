526777a65ef49ef595c0344df3180a18e610fce9
Kishin <kishin@trustless-services.com>
2021-01-14 18:56:45 +0900

    Initial commit

---
+++ routes/opennode.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+const opennodeController = require('../controllers/opennode')
+
+/* GET checkout page */
+router.get('/', async (req, res, next) => {
+  try {
+    amount = process.env.MIN_PAYMENT
+    success_url = process.env.HEROKU_URL === '' ? `localhost:${PORT || 3000}` : process.env.HEROKU_URL
+    if (req.body.amount < process.env.MIN_PAYMENT) return res.status(500)
+    const charge = await opennodeController.createCharge({ amount, success_url })
+    return res.redirect(`https://checkout.opennode.com/${charge.id}`)
+  } catch (err) {
+    console.error(err)
+  }
+})
+
+/* POST callback when invoice paid */
+router.post('/callback', async (req, res, next) => {
+  try {
+    const charge = req.body
+    const isValid = await opennodeController.signatureIsValid(charge)
+
+    if (isValid) {
+      console.log('Payment received at OpenNode')
+    }
+  } catch (err) {
+    console.error(err)
+  }
+})
+
+module.exports = router
